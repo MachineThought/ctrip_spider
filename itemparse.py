7baceb0c5dd86f7f13e12dfc9087df18b06fde82
MachineThought <machine_thought@163.com>
2017-05-04 23:18:59 +0800

    update

--- itemparse.py
+++ itemparse.py
@@ -30,17 +30,26 @@ class ItemParse:
     def parse_page(self, page):
         bs = BeautifulSoup(page, 'html.parser')
         textarea = bs.select("textarea")
+        # print(textarea)
+
         item_list = []
         if textarea is not None and textarea != "":
             for item in textarea:
+                # print(item)
                 item = str(item.text).strip()
+                # print(item)
                 if item == "":
                     continue
                 item = json.loads(item)
                 item_url = item["Url"]
                 if item_url.find("/") == 0:
-                    continue
+                    if item_url.find("vacations") < 0:
+                        continue
+                    else:
+                        item_url = "http:" + item_url
                 item_list.append(item_url)
+                # print(item_list)
+                # time.sleep(100)
         return item_list
 
     def main(self):
@@ -54,6 +63,7 @@ class ItemParse:
         for page_item in page_list:
             print(page_item)
             self.driver.get(page_item)
+            # time.sleep(2)
             page = self.driver.page_source
             travel_list = self.parse_page(page)
             for travel_item in travel_list:
@@ -79,20 +89,20 @@ class ItemParse:
 
 if __name__ == "__main__":
     dest_url = ['http://vacations.ctrip.com/tours/d-sanya-61', 'http://vacations.ctrip.com/tours/d-haikou-37',
-                'http://vacations.ctrip.com/tours/s-sanya-10558612',
-                'http://vacations.ctrip.com/tours/s-sanya-10558611',
+                'http://vacations.ctrip.com/tours/s-sanya-10558612', 'http://vacations.ctrip.com/tours/s-sanya-10558611',
                 'http://vacations.ctrip.com/tours/s-sanya-88071', 'http://vacations.ctrip.com/tours/s-sanya-10558614',
-                'http://vacations.ctrip.com/tours/s-sanya-10524171', 'http://vacations.ctrip.com/tours/d-xishaqundao-530',
-                'http://vacations.ctrip.com/tours/d-xiamen-21', 'http://vacations.ctrip.com/tours/d-fuzhou-164',
-                'http://vacations.ctrip.com/tours/d-gulangyu-120058', 'http://vacations.ctrip.com/tours/s-longyan-13293683',
-                'http://vacations.ctrip.com/tours/d-wuyishan-22', 'http://vacations.ctrip.com/tours/s-xiamen-91537',
-                'http://vacations.ctrip.com/tours/s-xiamen-79124', 'http://vacations.ctrip.com/tours/s-najing-90729',
-                'http://vacations.ctrip.com/tours/d-lijiang-32', 'http://vacations.ctrip.com/tours/d-kunming-29',
-                'http://vacations.ctrip.com/tours/d-xianggelila-106', 'http://vacations.ctrip.com/tours/d-xishuangbanna-30',
-                'http://vacations.ctrip.com/tours/d-luguhu-105', 'http://vacations.ctrip.com/tours/d-tengchong-696',
-                'http://vacations.ctrip.com/tours/d-ruili-1213', 'http://vacations.ctrip.com/tours/s-lijiang-75919',
-                'http://vacations.ctrip.com/tours/d-dali-31', 'http://vacations.ctrip.com/tours/d-honghe-512']
-    dest_url_3 = ['http://vacations.ctrip.com/tours/d-guilin-28', 'http://vacations.ctrip.com/tours/s-guilin-75898',
+                'http://vacations.ctrip.com/tours/s-sanya-10524171', 'http://vacations.ctrip.com/tours/d-xishaqundao-530']
+    dest_url_2_1 = ['http://vacations.ctrip.com/tours/d-xiamen-21', 'http://vacations.ctrip.com/tours/d-fuzhou-164',
+                    'http://vacations.ctrip.com/tours/d-gulangyu-120058', 'http://vacations.ctrip.com/tours/s-longyan-13293683',
+                    'http://vacations.ctrip.com/tours/d-wuyishan-22', 'http://vacations.ctrip.com/tours/s-xiamen-91537',
+                    'http://vacations.ctrip.com/tours/s-xiamen-79124', 'http://vacations.ctrip.com/tours/s-najing-90729']
+    dest_url_2 = ['http://vacations.ctrip.com/tours/d-lijiang-32', 'http://vacations.ctrip.com/tours/d-kunming-29',
+                  'http://vacations.ctrip.com/tours/d-xianggelila-106']
+    server_1 = ['http://vacations.ctrip.com/tours/d-guilin-28', 'http://vacations.ctrip.com/tours/d-xishuangbanna-30',
+                  'http://vacations.ctrip.com/tours/d-luguhu-105', 'http://vacations.ctrip.com/tours/d-tengchong-696',
+                  'http://vacations.ctrip.com/tours/d-ruili-1213', 'http://vacations.ctrip.com/tours/s-lijiang-75919',
+                  'http://vacations.ctrip.com/tours/d-dali-31', 'http://vacations.ctrip.com/tours/d-honghe-512',
+                  'http://vacations.ctrip.com/tours/s-guilin-75898',
                   'http://vacations.ctrip.com/tours/d-beihai-140', 'http://vacations.ctrip.com/tours/d-yangshuo-702',
                   'http://vacations.ctrip.com/tours/d-nanning-166', 'http://vacations.ctrip.com/tours/d-longjititian-970',
                   'http://vacations.ctrip.com/tours/d-weizhouisland-120063', 'http://vacations.ctrip.com/tours/d-jiuzhaigou-25',
@@ -104,8 +114,8 @@ if __name__ == "__main__":
                   'http://vacations.ctrip.com/tours/s-beijing-75594', 'http://vacations.ctrip.com/tours/s-beijing-75595',
                   'http://vacations.ctrip.com/tours/s-beijing-75598', 'http://vacations.ctrip.com/tours/s-beijing-10523549',
                   'http://vacations.ctrip.com/tours/s-beijing-75597', 'http://vacations.ctrip.com/tours/s-yanqing-75596',
-                  'http://vacations.ctrip.com/tours/s-beijing-76681', 'http://vacations.ctrip.com/tours/s-beijing-75599',
-                  'http://vacations.ctrip.com/tours/d-yantai-170', 'http://vacations.ctrip.com/tours/d-weihai-169',
+                  'http://vacations.ctrip.com/tours/s-beijing-76681', 'http://vacations.ctrip.com/tours/s-beijing-75599']
+    server_2 = ['http://vacations.ctrip.com/tours/d-yantai-170', 'http://vacations.ctrip.com/tours/d-weihai-169',
                   'http://vacations.ctrip.com/tours/d-jinan-128', 'http://vacations.ctrip.com/tours/d-qingdao-5',
                   'http://vacations.ctrip.com/tours/d-taishan-6', 'http://vacations.ctrip.com/tours/d-rizhao-622',
                   'http://vacations.ctrip.com/tours/s-qingdao-85745', 'http://vacations.ctrip.com/tours/d-zaozhuang-656',
@@ -120,7 +130,8 @@ if __name__ == "__main__":
                   'http://vacations.ctrip.com/tours/d-changbaishan-268', 'http://vacations.ctrip.com/tours/s-dalian-87618',
                   'http://vacations.ctrip.com/tours/d-dalian-4', 'http://vacations.ctrip.com/tours/d-changchun-216',
                   'http://vacations.ctrip.com/tours/d-haerbin-151', 'http://vacations.ctrip.com/tours/d-wudalianchi-857',
-                  'http://vacations.ctrip.com/tours/d-yabuli-815', 'http://vacations.ctrip.com/tours/d-jilin-100031']
+                  'http://vacations.ctrip.com/tours/d-yabuli-815', 'http://vacations.ctrip.com/tours/d-jilin-100031',
+                  'http://vacations.ctrip.com/tours/r-axnt-114', 'http://vacations.ctrip.com/tours/r-zhongdongfei-113']
 
     dest_url_4 = ['http://vacations.ctrip.com/tours/d-xian-7', 'http://vacations.ctrip.com/tours/d-yanan-423',
                   'http://vacations.ctrip.com/tours/d-huashan-183', 'http://vacations.ctrip.com/tours/s-xian-75682',
@@ -140,12 +151,11 @@ if __name__ == "__main__":
                   'http://vacations.ctrip.com/tours/d-macau-39', 'http://vacations.ctrip.com/tours/d-taiwan-100076',
                   'http://vacations.ctrip.com/tours/d-japan-100041', 'http://vacations.ctrip.com/tours/d-mongolia-20391',
                   'http://vacations.ctrip.com/tours/r-dongnanya-110', 'http://vacations.ctrip.com/tours/r-nanya-109',
-                  'http://vacations.ctrip.com/tours/r-europe-120002', 'http://vacations.ctrip.com/tours/r-meizhou-102',
-                  'http://vacations.ctrip.com/tours/r-axnt-114', 'http://vacations.ctrip.com/tours/r-zhongdongfei-113']
+                  'http://vacations.ctrip.com/tours/r-europe-120002', 'http://vacations.ctrip.com/tours/r-meizhou-102']
     chrome = webdriver.Chrome()
     # firefox = webdriver.Firefox()
     # firefox.find_element_by_id("").text
-    for url in dest_url:
+    for url in dest_url_2:
         itemParse = ItemParse(url, chrome)
         itemParse.main()
     print("end")
